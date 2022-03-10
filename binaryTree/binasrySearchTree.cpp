b8873b071fae873e5311951a30657c80f434a45e
Kartikey Dubey <dkartikey1@gmail.com>
2022-03-10 18:50:07 +0530

    added deletion in BST

--- binaryTree/binasrySearchTree.cpp
+++ binaryTree/binasrySearchTree.cpp
@@ -35,9 +35,61 @@ Node *insert(Node *node, int data)
     }
 }
 
-// preOrder
+// inOrder_Successor
+Node *inOrder_Successor(Node *node)
+{
+    Node *newVal = node;
+    while (newVal->left != NULL)
+    {
+        newVal = newVal->left;
+    }
+    return newVal;
+}
+//  Deletion ->
+Node *
+deleteNode(Node *root, int data)
+{
+    // Checking that root is not NUll
+    if (root == NULL)
+    {
+        return root;
+    }
 
-int preOrder(Node *node)
+    // Checking at which side data is
+    if (data < root->key)
+    {
+        root->left = deleteNode(root->left, data);
+    }
+    else if (data > root->key)
+    {
+        root->right = deleteNode(root->right, data);
+    }
+    // data mil gya final now considering 3 cases simultaneously 0 node , 1 node , 2 node
+    else
+    {
+        // 0 and 1 Node
+        if (root->left == NULL)
+        {
+            Node *temp = root->right;
+            free(root);
+            return temp;
+        }
+        else if (root->right == NULL)
+        {
+            Node *temp = root->left;
+            free(root);
+            return temp;
+        }
+        // 2 Node
+        Node *temp = inOrder_Successor(root->right);
+        root->key = temp->key;
+        root->right = deleteNode(root->right, temp->key);
+    }
+    return root;
+}
+
+// preOrder
+void preOrder(Node *node)
 {
     if (node != NULL)
     {
